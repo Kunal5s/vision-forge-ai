55ecde14cec2088b39c304cc64a68eb39e5c9f5b
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-06-21 16:20:10 +0000

    Not solved this problem redeploy again netlify This project but not opti

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,8 @@
 
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect } from 'react';
 import { notFound, useParams } from 'next/navigation';
 import { articles, type Article } from '@/lib/blog-data';
