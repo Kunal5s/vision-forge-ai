34341154562a86131cc095ae3094c7a8fe96565b
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-07-01 14:17:56 +0000

    2025-07-01T14:15:09.726678Z	Cloning repository... 2025-07-01T14:15:10.37

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,1 @@
-
-import { type MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-    sitemap: 'https://imagenbrainai.in/sitemap.xml',
-  };
-}
+export {};
